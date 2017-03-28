cdff51c6d3edad499606d82116e3d01e2c73edd0
Hugo LAUSENAZ-PIRE <hugo.lausenazpire@epsi.fr>
2017-03-28 23:26:37 +0200

    WeatherTest

---
+++ WeatherApp/WeatherPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace WeatherApp
+{
+    public partial class WeatherPage : ContentPage
+    {
+        public WeatherPage()
+        {
+            InitializeComponent();
+            this.Title = "Application Météo";
+            getWeatherBtn.Clicked += GetWeatherBtn_Clicked;
+
+            //Set the default binding to a default object for now
+            this.BindingContext = new Weather();
+        }
+
+        private async void GetWeatherBtn_Clicked(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(zipCodeEntry.Text))
+            {
+                Weather weather = await Core.GetWeather(zipCodeEntry.Text);
+                if (weather != null)
+                {
+                    this.BindingContext = weather;
+                    getWeatherBtn.Text = "Chercher encore";
+                }
+            }
+        }
+    }
+}
